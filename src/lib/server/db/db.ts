e116ab43e0ef4312cf486ce247926a8d05577e58
Fernando Jimenez <ferjip23@gmail.com>
2024-05-21 12:30:17 -0600

    env variables chenged

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
+import { POSTGRES_URL } from '$env/static/private';
 
-const pgClient = postgres("postgresql://hnosjotape:unEOwWF6gqX9@ep-lucky-pond-a52sle5e.us-east-2.aws.neon.tech/xeroweb_test?sslmode=require")
+const pgClient = postgres(POSTGRES_URL)
 export const db = drizzle(pgClient)
